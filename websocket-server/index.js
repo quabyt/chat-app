91295666f23ae2e286d19071fc70c4a0d287103c
Hasan Gül <hsngl695@gmail.com>
2025-08-06 11:59:54 +0300

    first commit

---
+++ websocket-server/index.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const { Client } = require('pg');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000", // React uygulamanın çalıştığı port
+        methods: ["GET", "POST"]
+    }
+});
+
+// PostgreSQL bağlantısı
+const db = new Client({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'chatapp',
+    password: '121212',
+    port: 5432,
+});
+
+db.connect()
+    .then(() => console.log('PostgreSQL bağlantısı başarılı'))
+    .catch((err) => console.error('PostgreSQL bağlantı hatası:', err.stack));
+
+const nicknames = new Set();
+
+io.on('connection', (socket) => {
+    console.log('Yeni bağlantı:', socket.id);
+
+    socket.on('join', (nickname) => {
+        if (nicknames.has(nickname)) {
+            socket.emit('join-error', 'Bu kullanıcı adı zaten kullanılıyor.');
+            return;
+        }
+
+        socket.nickname = nickname;
+        nicknames.add(nickname);
+        console.log(`${nickname} sohbete katıldı.`);
+
+        io.emit('user-joined', nickname);
+    });
+
+    socket.on('message', (msg) => {
+        const time = new Date();
+
+        db.query(
+            'INSERT INTO chat_messages (nickname, message, sent_at, created_at, updated_at) VALUES ($1, $2, $3, $4, $4)',
+            [socket.nickname, msg, time, time]
+        )
+            .then(() => {
+                console.log(`[DB] Mesaj eklendi: ${socket.nickname}: ${msg}`);
+            })
+            .catch((err) => {
+                console.error(`[DB HATA] Mesaj eklenemedi: ${err.message}`);
+            });
+
+        const mesajObjesi = {
+            nickname: socket.nickname,
+            message: msg,
+            time,
+        };
+
+        console.log('[SOCKET] Mesaj yayınlandı:', mesajObjesi);
+
+        io.emit('message', mesajObjesi);
+    });
+
+    socket.on('disconnect', () => {
+        if (socket.nickname) {
+            nicknames.delete(socket.nickname);
+            io.emit('user-left', socket.nickname);
+            console.log(`${socket.nickname} ayrıldı.`);
+        }
+    });
+});
+
+server.listen(3001, () => {
+    console.log('WebSocket sunucusu çalışıyor: http://localhost:3001');
+});
